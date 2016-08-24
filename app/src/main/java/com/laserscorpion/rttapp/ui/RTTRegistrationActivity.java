b81d705609c203afb1258d4eaabff8db71131b43
Joel Cretan <jcretan@gmail.com>
2016-08-24 16:16:57 -0400

    adding settings menu to RTTRegistrationActivity
    making settings more accessible since it is now more likely to want to change them after registering (with the new option to enable/disable real-time text)

--- app/src/main/java/com/laserscorpion/rttapp/ui/RTTRegistrationActivity.java
+++ app/src/main/java/com/laserscorpion/rttapp/ui/RTTRegistrationActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -198,6 +200,34 @@ public class RTTRegistrationActivity extends AppCompatActivity implements TextLi
     @Override
     public synchronized void dialogFail() {
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    private void launchSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            launchSettings();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
