a9be7dea44d8ce315a894c7ce2079010377ffa05
Joel Cretan <jcretan@gmail.com>
2016-09-09 15:41:15 -0400

    move javadoc output to gh-pages branch

--- app/src/main/java/com/laserscorpion/rttapp/sip/CallReceiver.java
+++
@@ -1,9 +0,0 @@
-package com.laserscorpion.rttapp.sip;
-
-public interface CallReceiver {
-    /**
-     * Called when a new call is coming in. The receiver must then accept or decline the call
-     * via the SipClient.
-     */
-    void callReceived(String from);
-}
